98bc7fc562a0cf17d5d771f70c6ef00f0f0aed4c
buczaq <226153@student.pwr.edu.pl>
2016-04-07 20:28:35 +0200

    Rozwiązanie

--- main.cpp
+++ main.cpp
@@ -7,17 +7,31 @@ void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
+	srand(atoi(argv[2]));
+	int n = atoi(argv[1]);
 	drawCharSquare(charSquare(n), n);	
 }
 
 char **charSquare(int n)
 {
-	char **square;
+	char **square = new char*[n];
+	for(int i=0; i<n; i++)
+		square[i] = new char[n];
+	for(int x=0; x<(n*n); x++)
+		square[x%n][x/n] = ('a' + rand()%('z'-'a'));
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
-
+	for(int x=0; x<(n*n); x++)
+		printf("%c%c", square[x%n][x/n], ((x%n)==(n-1))?10:' ');
 }
+/*
+uzupełnij niniejszy kod, w funkcji charSquare tworząc, a w funkcji drawCharSquare wyświetlając dwuwymiarową, dynamicznie alokowaną tablicę znaków,
+wielkość kwadratu pobieraj z parametru wiersza poleceń,
+znaki w tablicy mają być generowane losowo, przy użyciu funkcji rand(),
+nie możesz modyfikować prototypów funkcji. */
+/*
+tak wypełnianie, jak wyświetlanie tablicy square zrealizuj przy użyciu pojedynczej
+ pętli for, ograniczając jej ciało do jednej instrukcji.*/
